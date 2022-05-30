05945b0a1d04204cb00d0c33285d8fe532755c0c
Ayham Al-Ali <alali_ayham@yahoo.com>
2022-05-30 03:22:05 +0300

    V1.1

--- examples/facerec_from_webcam_faster.py
+++ examples/facerec_from_webcam_faster.py
@@ -1,9 +1,23 @@
 import face_recognition
 import cv2
 import numpy as np
+from datetime import datetime
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
+
+#!! Facial Recognition for Universities (FRU)
+#!! Artificial Intelligence Project
+#!! 
+#!! Team: Ayham Al-Ali & Ali-Al-Qaisi
+#!! 
+#!! Features:
+#!! 1- Detect if student is Present, Late or Absent (Absent by default) - ('Excused' status is manually assigned by Doctor)
+#!! 2- After 5 minutes of when the application starts (in-other-words when the class begins) the student will be marked as Late
+#!! 3- Before the first 5 minutes the studednt will be marked as Present
+#!! 4- Logs the enter time of each student
+#!! 
+
+
+# This is a demo includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
 #   2. Only detect faces in every other frame of video.
 
@@ -11,8 +25,44 @@ import numpy as np
 # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
 # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
-# Get a reference to webcam #0 (the default one)
-# video_capture = cv2.VideoCapture(0)
+
+
+
+
+
+
+#
+# Options
+#
+late_after_time = 5 # by minutes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# Code - DO NOT EDIT UNLESS YOU KNOW WHAT YOU'RE DOING
+#
+
+current_app_time = now = datetime.now()
+
+
+#! Get a reference to webcam #0 (the default one)
+#! video_capture = cv2.VideoCapture(0)
+
+#! Ghangeable based on the external camera I use since my default webcam (laptop's) is broken
+#! Check the number by checking available `video` in `/dev/videoX` where X is the number
 video_capture = cv2.VideoCapture(2)
 
 # Create arrays of known face encodings and their names
@@ -29,15 +79,12 @@ def addKnownFace(file_name, person_name):
 
 known_faces_folder_path = "./known_people/"
 
+# Add faces here
 addKnownFace(known_faces_folder_path + "Ayham.jpg", "Ayham")
 addKnownFace(known_faces_folder_path + "Fahad.jpg", "Fahad")
 addKnownFace(known_faces_folder_path + "Miar.jpg", "Miar")
 addKnownFace(known_faces_folder_path + "Ammar.jpg", "Ammar")
 addKnownFace(known_faces_folder_path + "Zaid.jpg", "Zaid")
-
-# Load a second sample picture and learn how to recognize it.
-ayham_image = face_recognition.load_image_file("Ayham.jpg")
-ayham_face_encoding = face_recognition.face_encodings(ayham_image)[0]
 
 
 # Initialize some variables
@@ -56,6 +103,8 @@ while True:
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
 
+    # face_status = []
+
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
@@ -66,7 +115,7 @@ while True:
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+            name = "0x0" # ;)
 
             # # If a match was found in known_face_encodings, just use the first one.
             # if True in matches:
@@ -79,7 +128,20 @@ while True:
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
 
+            # if (datetime.now() - current_app_time).total_seconds() > late_after_time * 60:
+            #     face_status.append("Late")
+            # else:
+            #     face_status.append("Present")
+
+            if (datetime.now() - current_app_time).total_seconds() > late_after_time * 60:
+                name += ", s: Late"
+            elif name != "0x0":
+                name += ", s: Present"
+            else:
+                name += ", s: 0x0"
+
             face_names.append(name)
+
 
     process_this_frame = not process_this_frame
 
@@ -92,6 +154,9 @@ while True:
         bottom *= 4
         left *= 4
 
+        status = name.split(", s: ")[1]
+        name = name.split(", s: ")[0]
+
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
@@ -100,8 +165,21 @@ while True:
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
+        # Draw a second label with the status (Late, Present)
+        status_color = []
+        if status == "Present":
+            status_color = [0, 200, 0]
+        elif status == "Late":
+            status_color = [255, 165, 0]
+        else:
+            status_color = [224, 73, 62]
+
+        cv2.rectangle(frame, (left, bottom + 35), (right, bottom), (status_color[0], status_color[1], status_color[2]), cv2.FILLED)
+        cv2.putText(frame, status, (left + 6, bottom + 35 - 6), font, 1.0, (255, 255, 255), 1)
+
+
     # Display the resulting image
-    cv2.imshow('Video', frame)
+    cv2.imshow('FRU - AI Project - AA Team', frame)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
